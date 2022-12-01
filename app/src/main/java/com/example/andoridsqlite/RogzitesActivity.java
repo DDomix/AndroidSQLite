04392825111f43e8a71cb3b44f95c0e33f581f31
DDomix <dominik.papp1010@gmail.com>
2022-12-01 15:28:19 +0100

    best gypsy man is KUNU MARIO

--- app/src/main/java/com/example/andoridsqlite/RogzitesActivity.java
+++ app/src/main/java/com/example/andoridsqlite/RogzitesActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RogzitesActivity extends AppCompatActivity {
 
@@ -15,6 +16,8 @@ public class RogzitesActivity extends AppCompatActivity {
     private EditText jegy;
     private Button rogzitesbutton;
     private Button backbutton;
+
+    private DBHelper adatbazis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,25 @@ public class RogzitesActivity extends AppCompatActivity {
                 finish();
             }
         });
+        rogzitesbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String veznev=vezeteknev.getText().toString().trim();
+                String kernev=keresztnev.getText().toString().trim();
+                String jeggy=jegy.getText().toString().trim();
+                if (veznev.isEmpty()|| kernev.isEmpty()||jeggy.isEmpty()){
+                    Toast.makeText(RogzitesActivity.this, "Minden mező kitöltése kötelező", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    int jegy=Integer.parseInt(jeggy);
+                    if (adatbazis.rogzites(veznev,kernev,jegy)){
+                        Toast.makeText(RogzitesActivity.this, "Sikeres adatrögzítés", Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(RogzitesActivity.this, "Sikertelen adatrögzítés", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
     private void init(){
         vezeteknev=findViewById(R.id.vezeteknev);
@@ -35,5 +57,6 @@ public class RogzitesActivity extends AppCompatActivity {
         jegy=findViewById(R.id.jegy);
         rogzitesbutton=findViewById(R.id.rogzitesbutton);
         backbutton=findViewById(R.id.backbutton);
+        adatbazis = new DBHelper(RogzitesActivity.this);
     }
 }
