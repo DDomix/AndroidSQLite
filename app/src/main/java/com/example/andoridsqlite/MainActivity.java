2a596e31e3ed8619f479b6197e2a5df6c1630196
DDomix <dominik.papp1010@gmail.com>
2022-12-01 15:18:11 +0100

    best gypsy man is KUNU

--- app/src/main/java/com/example/andoridsqlite/MainActivity.java
+++ app/src/main/java/com/example/andoridsqlite/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.andoridsqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     private Button changebutton;
     private Button deletebutton;
     private TextView textView;
+
+    private DBHelper adatbazis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +33,37 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+        readbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor adatok = adatbazis.listaz();
+                if (adatok.getCount() == 0) {
+                    Toast.makeText(MainActivity.this, "Nincs az adatbázisban bejegyzés", Toast.LENGTH_SHORT).show();
+                } else {
+                    StringBuilder bobTheBuilder = new StringBuilder();
+                    while (adatok.moveToNext()){
+                        bobTheBuilder.append("ID: ").append(adatok.getInt(0));
+                        bobTheBuilder.append(System.lineSeparator());
+                        bobTheBuilder.append("Vezetéknév ").append(adatok.getString(1));
+                        bobTheBuilder.append(System.lineSeparator());
+                        bobTheBuilder.append("Keresztnév ").append(adatok.getString(2));
+                        bobTheBuilder.append(System.lineSeparator());
+                        bobTheBuilder.append("Jegy ").append(adatok.getString(3));
+                        bobTheBuilder.append(System.lineSeparator());
+                        bobTheBuilder.append(System.lineSeparator());
+                    }
+                    textView.setText(bobTheBuilder.toString());
+                }
+            }
+        });
     }
-    private void init(){
-        readbutton=findViewById(R.id.readbutton);
-        addbutton=findViewById(R.id.addbutton);
-        changebutton=findViewById(R.id.changebutton);
-        deletebutton=findViewById(R.id.deletebutton);
-        textView=findViewById(R.id.textview);
+
+    private void init() {
+        readbutton = findViewById(R.id.readbutton);
+        addbutton = findViewById(R.id.addbutton);
+        changebutton = findViewById(R.id.changebutton);
+        deletebutton = findViewById(R.id.deletebutton);
+        textView = findViewById(R.id.textview);
+        adatbazis = new DBHelper(MainActivity.this);
     }
 }
